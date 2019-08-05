9b18ededaf930771ce61234a2499aef6f157de0c
Julian <1464861542@qq.com>
2019-08-05 12:42:43 +0800

    添加选择排序

--- Sortings.py
+++ Sortings.py
@@ -35,10 +35,27 @@ def short_bubble_sort(ul):
         p -= 1
 
 
+def selection_sort(ul):
+    """选择排序
+
+    :param ul: List[int]
+    :return: List[int]
+    """
+    for tar_pos in range(len(ul) - 1, 0, -1):  # tar_pos是此次pass中最大值应该在的位置
+        pos_of_max = 0  # 假设此次pass最大值的位置为索引0
+        for i in range(1, tar_pos + 1):  # 对于剩下的所有索引，都要遍历一遍与‘最大值’比较
+            if ul[i] > ul[pos_of_max]:  # 如果大，就更新最大值索引
+                pos_of_max = i
+        # 最后把最大值位置和目标位置换一下
+        ul[pos_of_max], ul[tar_pos] = ul[tar_pos], ul[pos_of_max]
+
+
 if __name__ == '__main__':
     unordered_ls = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-    # ls2 = [19, 1, 9, 7, 3, 10, 13, 15, 8, 12]
+    # check_ls = [19, 1, 9, 7, 3, 10, 13, 15, 8, 12]
     nearly_sorted_ls = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]  # 有序度高
+    check_ls2 = [11, 7, 12, 14, 19, 1, 6, 18, 8, 20]
     # bubble_sort(unordered_ls)
     # short_bubble_sort(nearly_sorted_ls)
-    # print(nearly_sorted_ls)
+    selection_sort(check_ls2)
+    print(check_ls2)
