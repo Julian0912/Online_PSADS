8d56e7d3c003899af5f4a04facdb9956310390a9
Julian <1464861542@qq.com>
2019-08-05 13:12:18 +0800

    添加插入排序

--- Sortings.py
+++ Sortings.py
@@ -50,6 +50,22 @@ def selection_sort(ul):
         ul[pos_of_max], ul[tar_pos] = ul[tar_pos], ul[pos_of_max]
 
 
+def insertion_sort(ul):
+    """插入排序
+
+    :param ul: List[int]
+    :return: List[int]
+    """
+    for index in range(1, len(ul)):  # 假设数组第一个值为一个有序数组，则从第二个值开始
+        cur = ul[index]  # 存储当前值
+        pos = index  # 向前跑动的游标
+        while pos > 0 and ul[pos - 1] > cur:  # 在游标到头之前，如果游标前一个值比当前值大
+            ul[pos] = ul[pos - 1]  # 就把前一个值后移一位（覆盖了当前值，留出了一个空位）
+            pos -= 1  # 游标前移一位
+        ul[pos] = cur  # 空位会一直向后移动，直到游标前一个值比当前值小，则把当前值插入空位
+        # 注：空位实际还是有值
+
+
 if __name__ == '__main__':
     unordered_ls = [54, 26, 93, 17, 77, 31, 44, 55, 20]
     # check_ls = [19, 1, 9, 7, 3, 10, 13, 15, 8, 12]
@@ -57,5 +73,6 @@ if __name__ == '__main__':
     check_ls2 = [11, 7, 12, 14, 19, 1, 6, 18, 8, 20]
     # bubble_sort(unordered_ls)
     # short_bubble_sort(nearly_sorted_ls)
-    selection_sort(check_ls2)
-    print(check_ls2)
+    # selection_sort(check_ls2)
+    insertion_sort(unordered_ls)
+    print(unordered_ls)
